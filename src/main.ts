7769d18e1b767ccfbd19b925a31fdb2efcc340ea
David Négrier <d.negrier@thecodingmachine.com>
2024-07-02 11:12:32 +0200

    First bot

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,34 @@ console.log('Script started successfully');
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
+WA.onInit().then(async () => {
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
+    await WA.players.configureTracking({
+        players: true,
+        movement: false,
+    });
+
+
+    if (WA.room.hashParameters.bot) {
+        console.log('Bot detected');
+        initBot();
+    }
+
 }).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function initBot() {
+    WA.player.proximityMeeting.onJoin().subscribe((players) => {
+        console.log('BUBBLE DETECTED', players);
+        WA.chat.sendChatMessage('Hello there!', { scope: 'bubble' });
+    });
+
+    WA.chat.onChatMessage((message) => {
+        console.log('Chat message received', message);
+    });
 }
 
 export {};
